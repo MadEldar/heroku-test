d29c3616ac308012df61f91a1093415a4e84b644
MadEldar1 <markskilink@gmail.com>
2020-03-20 17:24:55 +0700

    Added cart and order

--- database/seeds/BrandSeeder.php
+++ database/seeds/BrandSeeder.php
@@ -11,6 +11,17 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Brand::class, 10)->create();
+        \Illuminate\Support\Facades\DB::statement("INSERT INTO laravel.brands (brand_name,created_at,updated_at) VALUES
+        ('Dr. Caroline Corkery','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Allie Ullrich II','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Prof. Raven Hartmann III','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Dr. Dewitt Kilback','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Mr. Richie Ryan I','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Jalyn Skiles Jr.','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Dr. Mandy Weimann DVM','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Jovan Balistreri','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Nia Bartoletti','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ,('Edwardo Reichel MD','2020-03-05 15:58:17.0','2020-03-05 15:58:17.0')
+        ;");
     }
 }
