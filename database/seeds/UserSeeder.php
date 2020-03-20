813cff1f20163b2475f000e8e496af73f7175b62
MadEldar1 <markskilink@gmail.com>
2020-03-20 17:33:43 +0700

    Added cart and order

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,11 +11,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\Http\Controllers\User::create([
-            'email' => 'root@dailyshop.com',
-            'password' => '1',
-            'name' => 'admin',
-            'role' => 1
-        ]);
+        \Illuminate\Support\Facades\DB::statement("INSERT INTO users (name,email,password,role,remember_token,email_verified_at,deactivated_at,created_at,updated_at) VALUES
+        ('mark','root@dailyshop.com','1',1,'E68nPawnfascX8vqnHWbjuzuSAnLTvKFm3tAlrApzMvKFrfihR8Fe7rnKsLE','2020-03-19 14:44:09.0',NULL,'2020-03-19 07:44:09.0','2020-03-19 08:02:38.0');");
     }
 }
