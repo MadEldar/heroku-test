58031e95ac7f59905cd78afec693018dbe748950
MadEldar1 <markskilink@gmail.com>
2020-03-27 10:36:07 +0700

    Fixing mixed content error #1

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -4,15 +4,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title }}</title>
 
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/font-awesome.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/bootstrap.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/jquery.smartmenus.bootstrap.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/jquery.simpleLens.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/slick.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/nouislider.css") }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/theme-color/default-theme.css") }}" id="switcher">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/sequence-theme.modern-slide-in.css") }}" media="all">
-    <link type="text/css" rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/font-awesome.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/bootstrap.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/jquery.smartmenus.bootstrap.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/jquery.simpleLens.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/slick.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/nouislider.css") }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/theme-color/default-theme.css") }}" id="switcher">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/sequence-theme.modern-slide-in.css") }}" media="all">
+    <link type="text/css" rel="stylesheet" href="{{ asset("/css/style.css") }}">
 
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
